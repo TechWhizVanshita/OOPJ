6111ce8f9b95ec6de7e15928553703a40aa31dab
Vanshita <120383319+vanshita-80048@users.noreply.github.com>
2024-04-04 18:03:15 +0530

    add files

---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+class vansh {
+    public static void main(String a[]) {
+        System.out.println("Hello vanshita");
+        System.out.println(2 + 5);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Test {
+
+    public static void main(String args[]) {
+        int a, b;
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Enter a= ");
+        a = kb.nextInt();
+
+        System.out.print("Enter b= ");
+        b = kb.nextInt();
+
+        System.out.println(a + b);
+        System.out.println("sum= " + (a + b));
+
+        System.out.println((a + b));
+
+    }
+}
